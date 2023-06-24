f9310ebb9b02a5b8f81ec3ee13b1d30d2d6affef
Petros Kitazos <petroskitazos@pac.local>
2023-06-24 14:01:17 +0100

    initial commit

---
+++ src/routes/+page.svelte
@@ -0,0 +1,12 @@
+<script>
+	import Counter from '../lib/components/Counter.svelte';
+</script>
+
+<svelte:head>
+	<title>Home</title>
+	<meta name="description" content="Svelte demo app" />
+</svelte:head>
+
+<section>
+	<Counter />
+</section>
