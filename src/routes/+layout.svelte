b3238dc03efc55822a95333836ca234e1b655883
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2023-06-24 18:47:20 +0100

    did main index page

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,10 +1,8 @@
 <script>
-	import Header from '../lib/components/Header.svelte';
 	import '../app.css';
 </script>
 
 <div class="app">
-	<Header />
 	<main>
 		<slot />
 	</main>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,12 +1,17 @@
 <script>
-	import Counter from '../lib/components/Counter.svelte';
+	import ActionButton from '$lib/components/ActionButton.svelte';
 </script>
 
 <svelte:head>
-	<title>Home</title>
-	<meta name="description" content="Svelte demo app" />
+	<title>Unnamed Bingo Game</title>
+	<meta name="description" content="Who cares tbh" />
 </svelte:head>
 
-<section>
-	<Counter />
+<section class="grid place-items-center h-screen">
+	<div class="flex flex-col w-1/4 gap-5 items-center">
+		<ActionButton action="/room/create">Create</ActionButton>
+		<div class="divider">Or</div>
+		<ActionButton action="/room/join">Join</ActionButton>
+		<div class="font-accent text-3xl italic">a Room</div>
+	</div>
 </section>
