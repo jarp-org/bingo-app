f30a273871c86dedfc9df74b58f26fcc932a2bb3
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2023-06-24 18:46:33 +0100

    added action button

---
+++ src/lib/components/ActionButton.svelte
@@ -0,0 +1,13 @@
+<script lang="ts">
+	export let action: (() => void) | string;
+</script>
+
+{#if typeof action === 'string'}
+	<a class="btn-primary btn-outline btn w-full hover:scale-105" href={action}>
+		<slot />
+	</a>
+{:else}
+	<button class="btn-primary btn-outline btn w-full hover:scale-105" on:click={action}>
+		<slot />
+	</button>
+{/if}
