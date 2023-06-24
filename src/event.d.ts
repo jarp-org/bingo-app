866543d41fad66683da718b7f856c373a7f2fc60
Jacob Trevor <50667410+JakeTrevor@users.noreply.github.com>
2023-06-24 14:10:01 +0100

    added websocket event types

---
+++ src/event.d.ts
@@ -0,0 +1,66 @@
+interface Incident {
+	id: string;
+	label: string;
+	count: number;
+}
+
+interface Player {
+	id: string;
+	name: string;
+}
+
+interface addIncident {
+	type: 'addIncident';
+	incident: Incident;
+}
+
+interface removeIncident {
+	type: 'removeIncident';
+	incident: Incident;
+}
+
+interface incidentOccurrence {
+	type: 'incidentOccurrence';
+	incident: Incident;
+}
+
+interface playerJoin {
+	type: 'playerJoin';
+	player: Player;
+}
+
+interface playerLeave {
+	type: 'playerLeave';
+	player: Player;
+}
+
+interface gameStart {
+	type: 'gameStart';
+}
+
+interface gameEnd {
+	type: 'gameEnd';
+}
+
+interface gameWon {
+	type: 'gameWon';
+	player: Player;
+}
+
+interface handshake {
+	type: 'handshake';
+	players: Player[];
+	incidents: Incident[];
+	code: string;
+}
+
+type event =
+	| addIncident
+	| removeIncident
+	| incidentOccurrence
+	| playerJoin
+	| playerLeave
+	| gameStart
+	| gameEnd
+	| gameWon
+	| handshake;
